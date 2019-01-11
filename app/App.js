e21a9172be6bef8fb0a422b59c283a745a8c3058
Maya Vera <maya@mayavera.me>
2019-01-10 21:53:41 -0500

    created simple ball physics

--- app/App.js
+++ app/App.js
@@ -1,12 +1,105 @@
 import React, { Component } from 'react'
 import c from './App.scss'
 
-const playerHeight = 50;
-const playerWidth = 200;
+const playerHeight = 20
+const playerWidth = 100
+const ballRadius = 10
+const initialBallX = 600
+const initialBallY = 880
+const initialBallVX = 1
+const initialBallVY = 1
 
 export default class App extends Component {
   state = {
-    playerX: 0
+    playerX: playerWidth / 2,
+    playerY: playerHeight / 2,
+    ballX: initialBallX,
+    ballY: initialBallY,
+    ballVX: initialBallVX,
+    ballVY: initialBallVY
+  }
+
+  step = () => {
+    let { ballX, ballY, ballVX, ballVY, playerY, playerX, worldHeight, worldWidth } = this.state
+
+    let playerTop, playerBottom, playerLeft, playerRight
+    function refreshPlayer() {
+      playerTop = playerY + (playerHeight / 2)
+      playerLeft = playerX - (playerWidth / 2)
+      playerRight = playerX + (playerWidth / 2)
+    }
+    refreshPlayer()
+
+    let newBallX = ballX + ballVX
+    let newBallY = ballY + ballVY
+
+    let ballTop, ballBottom, ballRight, ballLeft
+    function refreshBall() {
+      ballTop = newBallY + ballRadius
+      ballBottom = newBallY - ballRadius
+      ballRight = newBallX + ballRadius
+      ballLeft = newBallX - ballRadius
+    }
+    refreshBall()
+
+    if (
+      ballBottom < playerTop
+      && ballVY < 0
+      && ballLeft < playerRight
+      && ballRight > playerLeft
+    ) {
+      ballVY *= -1
+      refreshBall()
+    }
+
+    if (ballBottom < 0 && ballVY < 0) {
+      ballVY *= -1
+      newBallY = ballRadius
+      refreshBall()
+    }
+
+    if (ballTop > worldHeight && ballVY > 0) {
+      ballVY *= -1
+      newBallY = worldHeight - ballRadius
+      refreshBall()
+    }
+
+    if (ballLeft < 0 && ballVX < 0) {
+      ballVX *= -1
+      newBallX = ballRadius
+      refreshBall()
+    }
+
+    if (ballRight > worldWidth && ballVX > 0) {
+      ballVX *= -1
+      newBallX = worldWidth - ballRadius
+      refreshBall()
+    }
+
+    this.setState({
+      ballX: newBallX,
+      ballY: newBallY,
+      ballVY,
+      ballVX
+    })
+  }
+
+  syncWorldDimensions = () => {
+    this.setState({
+      worldWidth: this.world.offsetWidth,
+      worldHeight: this.world.offsetHeight
+    })
+  }
+
+  componentDidMount() {
+    this.ticker = setInterval(this.step, 10)
+    this.resizeHandler = window.addEventListener('resize', this.syncWorldDimensions)
+    this.syncWorldDimensions()
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ticker)
+    window.removeEventListener(this.resizeHandler)
   }
 
   handleMouseMove = e => {
@@ -16,16 +109,30 @@ export default class App extends Component {
   }
 
   render() {
+    const { ballX, ballY, playerX, playerY } = this.state
+
     return (
       <div
         className={c.app}
-        onMouseMove={this.handleMouseMove}>
+        onMouseMove={this.handleMouseMove}
+        ref={element => { this.world = element }}
+      >
+        <div
+          className={c.ball}
+          style={{
+            width: ballRadius * 2,
+            height: ballRadius * 2,
+            left: ballX - ballRadius,
+            bottom: ballY - ballRadius
+          }}
+        />
         <div
           className={c.player}
           style={{
             width: playerWidth,
             height: playerHeight,
-            left: this.state.playerX - (playerWidth / 2)
+            left: playerX - (playerWidth / 2),
+            bottom: playerY - (playerHeight / 2)
           }}
         />
       </div>
